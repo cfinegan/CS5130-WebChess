afa53b37346cd840bb835bd7efcf7590159d5cd1
Conor Finegan <conor_finegan@student.uml.edu>
2019-11-22 22:06:06 -0500

    more logic for board-click

--- src/cljs/chess/match/events.cljs
+++ src/cljs/chess/match/events.cljs
@@ -2,6 +2,7 @@
   (:require
    [re-frame.core :as re-frame]
    [chess.chess :as chess]
+   [chess.match.subs :as subs]
    ))
 
 (defn reg-match-event-fx [id f]
@@ -16,4 +17,33 @@
 (reg-match-event-fx
  ::board-click
  (fn [cofx [_ x y]]
-   {:db (assoc (:db cofx) :selection {:pos (chess/->Coord x y)})}))
+   (let [history @(re-frame/subscribe [::subs/history])
+         board (:board (last history))
+         selection @(re-frame/subscribe [::subs/selection])
+         team @(re-frame/subscribe [::subs/team])
+         active-team @(re-frame/subscribe [::subs/active-team])
+         click-pos (chess/->Coord x y)]
+     (cond
+       ;; Do nothing if it's the opponent's turn.
+       ;; TODO: Add an invalid selection animation.
+       (not (= team active-team))
+       {:db (:db cofx)}
+       ;; If a piece is already selected...
+       selection
+       (let [{sel-pos :pos} selection]
+         (if (= sel-pos click-pos)
+           ;; Selecting the same piece twice de-selects.
+           {:db (assoc (:db cofx) :selection nil)}
+           ;; Otherwise do nothing
+           ;; TODO: try to move the piece here
+           {:db (:db cofx)}))
+       ;; Otherwise attempt to select.
+       :else
+       (let [piece (board click-pos)]
+         (if (and piece (= (:team piece) team))
+           ;; Only select when the space holds a friendly piece.
+           {:db (assoc (:db cofx) :selection {:pos click-pos})}
+           ;; Otherwise do nothing
+           ;; TODO: Add an invalid selection animation.
+           {:db (:db cofx)}))))))
+
